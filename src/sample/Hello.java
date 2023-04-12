14f1fb503547b4f55f9acda38a0002944340e011
sojung <sjleee406@naver.com>
2023-04-12 09:47:36 +0900

    A.java Hello.java 추가

--- src/sample/Hello.java
+++ src/sample/Hello.java
@@ -1,8 +1,10 @@
 package sample;
 
-public class Sample {
+public class Hello {
+
 	public static void main(String[] args) {
-		System.out.println("Hello, git!");
+		// TODO Auto-generated method stub
+
 	}
 
 }
